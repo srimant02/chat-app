546f9fb925aa3d03a6faa321b69dc1450284a968
srimant02 <srimantacharjya0203@gmail.com>
2024-10-31 14:59:57 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mern-chat-app-dbif.onrender.com", {
+			const socket = io("https://chat-app-u01g.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
